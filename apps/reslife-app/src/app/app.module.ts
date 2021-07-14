16c6418ce2ec1a05c130fd548bfb92211bd68e22
atinybeardedman <sedickinson9123@gmail.com>
2021-07-14 08:37:08 -0400

    feat: consume ui component in reslife-app

--- apps/reslife-app/src/app/app.module.ts
+++ apps/reslife-app/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SharedUiModule } from '@reslife/shared/ui';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot([
+  imports: [BrowserModule, BrowserAnimationsModule, SharedUiModule, RouterModule.forRoot([
     {
       path: '',
       pathMatch: 'full',
--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,9 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppShellModule } from './app-shell/app-shell.module';
+
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, AppShellModule],
+  declarations: [
+    TopBarComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [
+    TopBarComponent
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedUiModule {}
