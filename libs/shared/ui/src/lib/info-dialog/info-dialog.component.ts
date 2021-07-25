d0853cf6318b49b27ffa17ad3002187639779773
atinybeardedman <sedickinson9123@gmail.com>
2021-07-25 11:30:27 -0400

    feat: add info dialog

---
+++ libs/shared/ui/src/lib/info-dialog/info-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+interface InfoDialogData {
+  message: string;
+}
+
+@Component({
+  selector: 'reslife-info-dialog',
+  templateUrl: './info-dialog.component.html',
+  styleUrls: ['./info-dialog.component.scss']
+})
+export class InfoDialogComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: InfoDialogData) { }
+
+
+}
--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,18 +1,19 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainViewModule } from './main-view/main-view.module';
 import { SearchSelectModule } from './search-select/search-select.module';
+import { InfoDialogModule } from './info-dialog/info-dialog.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    // BrowserAnimationsModule,
     SearchSelectModule,
+    InfoDialogModule,
   ],
   exports: [
     MainViewModule,
-    SearchSelectModule
+    SearchSelectModule,
+    InfoDialogModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
