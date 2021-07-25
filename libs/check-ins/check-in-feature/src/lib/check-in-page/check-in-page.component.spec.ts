345bd9beb426faa3e8728d2d646ada8c2318492c
atinybeardedman <sedickinson9123@gmail.com>
2021-07-25 13:51:25 -0400

    fix: mock dep for check-in page component

--- libs/check-ins/check-in-feature/src/lib/check-in-page/check-in-page.component.spec.ts
+++ libs/check-ins/check-in-feature/src/lib/check-in-page/check-in-page.component.spec.ts
@@ -1,5 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CheckInDataService } from '@reslife/check-ins/check-in-data-access';
+import { SharedUiModule } from '@reslife/shared/ui';
 
 import { CheckInPageComponent } from './check-in-page.component';
 
@@ -9,14 +11,21 @@ describe('CheckInPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckInPageComponent ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-    .compileComponents();
+      imports: [SharedUiModule],
+      declarations: [CheckInPageComponent],
+      providers: [
+        {
+          provide: CheckInDataService,
+          useValue: jest.mock('@reslife/check-ins/check-in-data-access'),
+        },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CheckInPageComponent);
+    TestBed.inject(CheckInDataService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
