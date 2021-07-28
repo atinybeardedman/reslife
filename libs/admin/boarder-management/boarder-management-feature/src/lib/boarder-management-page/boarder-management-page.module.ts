9966fe7a2d28d2fd8d078c89398fcaf7def62275
atinybeardedman <sedickinson9123@gmail.com>
2021-07-28 11:46:16 -0400

    feat: add styling

--- libs/admin/boarder-management/boarder-management-feature/src/lib/boarder-management-page/boarder-management-page.module.ts
+++ libs/admin/boarder-management/boarder-management-feature/src/lib/boarder-management-page/boarder-management-page.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BoarderManagementPageComponent } from './boarder-management-page.component';
 import { AdminUiModule } from '@reslife/admin-ui';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
-  imports: [CommonModule, AdminUiModule, MatButtonModule],
+  imports: [CommonModule, AdminUiModule, MatButtonModule, MatCardModule, FlexLayoutModule],
   exports: [
     BoarderManagementPageComponent
   ],
