8b79e8f7ae873074dd8bdb2f5ff254e40846a96e
atinybeardedman <sedickinson9123@gmail.com>
2021-08-05 13:30:29 -0400

    feat: add layout of student-signout-page

--- libs/student-signout/student-signout-feature/src/lib/student-signout-page/student-signout-page.component.ts
+++ libs/student-signout/student-signout-feature/src/lib/student-signout-page/student-signout-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { BoarderSignoutMeta, StudentSignout } from '@reslife/student-signout/student-signout-model';
+import { StudentSignoutDataService } from '@reslife/student-signout/student-signout-data-access';
 @Component({
   selector: 'reslife-student-signout-page',
   templateUrl: './student-signout-page.component.html',
@@ -7,10 +9,25 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StudentSignoutPageComponent implements OnInit {
-
-  constructor() { }
+  signouts$!: Observable<StudentSignout[]>
+  boarders$!: Observable<BoarderSignoutMeta[]>;
+  selectedSignout!: StudentSignout | null; 
+  constructor(private ssd: StudentSignoutDataService) { }
 
   ngOnInit(): void {
+    this.boarders$ = this.ssd.getAvailableBoarders();
+    this.signouts$ = this.ssd.getCurrentSignouts();
   }
+
+
+
+  newSignout(): void {}
+  
+  editSignout(signout: StudentSignout): void {}
+
+  checkIn(signout: StudentSignout): void {}
+
+  saveSignout(signout: StudentSignout): void {}
+
 
 }
--- libs/student-signout/student-signout-feature/src/lib/student-signout-page/student-signout-page.module.ts
+++ libs/student-signout/student-signout-feature/src/lib/student-signout-page/student-signout-page.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentSignoutPageComponent } from './student-signout-page.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { StudentSignoutUiModule } from '@reslife/student-signout/student-signout-ui';
 
 @NgModule({
   declarations: [
     StudentSignoutPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    StudentSignoutUiModule
   ],
   exports: [
     StudentSignoutPageComponent
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class StudentSignoutPageModule { }
