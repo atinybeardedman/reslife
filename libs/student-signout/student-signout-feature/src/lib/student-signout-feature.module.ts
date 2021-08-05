d2e65b6e41a2c35e8a5f7b8de2c00bf53132e893
atinybeardedman <sedickinson9123@gmail.com>
2021-08-05 12:08:13 -0400

    fix: adjust paths and routes

--- libs/student-signout/student-signout-feature/src/lib/student-signout-feature.module.ts
+++ libs/student-signout/student-signout-feature/src/lib/student-signout-feature.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { StudentSignoutPageModule } from './student-signout-page/student-signout-page.module';
+import { StudentSignoutPageComponent } from './student-signout-page/student-signout-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ]),
     StudentSignoutPageModule,
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', component: StudentSignoutPageComponent}
+    ]),
   ],
+  exports: [StudentSignoutPageModule]
 })
 export class StudentSignoutFeatureModule {}
