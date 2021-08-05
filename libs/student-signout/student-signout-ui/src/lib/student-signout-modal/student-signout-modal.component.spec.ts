110bfa3a85a68ac127e99ffb7b86edd3d62680e4
atinybeardedman <sedickinson9123@gmail.com>
2021-08-05 16:03:24 -0400

    feat: create modal with initial assertions

--- libs/student-signout/student-signout-ui/src/lib/student-signout-modal/student-signout-modal.component.spec.ts
+++ libs/student-signout/student-signout-ui/src/lib/student-signout-modal/student-signout-modal.component.spec.ts
@@ -22,4 +22,27 @@ describe('StudentSignoutModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('before a student is selected',() => {
+    // it('should only show the name picker');
+    // it('should disable the signout button')
+  })
+
+  describe('When a not campused student is selected', () => {
+    // it('should show the form');
+    // it('should only show transportation methods the student has permissions for')
+    // it('should show the which car field when car is selected')
+    // it('should not allow signing out if the form is invalid')
+  });
+
+  describe('When a campused student is selected', () => {
+    // it('should show they are campused instead of the form');
+
+    // it('should disable the signout button');
+  });
+
+  describe('When editing a signout', () => {
+    // it('should not allow the user to change the student');
+    // it('should fill the fields with the given signout information');
+  })
 });
--- libs/student-signout/student-signout-ui/src/lib/student-signout-modal/student-signout-modal.component.ts
+++ libs/student-signout/student-signout-ui/src/lib/student-signout-modal/student-signout-modal.component.ts
@@ -1,16 +1,105 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NamedItem } from '@reslife/shared-models';
+import {
+  BoarderSignoutMeta,
+  StudentSignout,
+  TransportMethod,
+} from '@reslife/student-signout/student-signout-model';
 @Component({
   selector: 'reslife-student-signout-modal',
   templateUrl: './student-signout-modal.component.html',
   styleUrls: ['./student-signout-modal.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StudentSignoutModalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class StudentSignoutModalComponent implements OnChanges {
+  @Input() signout!: StudentSignout | null;
+  @Input() boarders!: BoarderSignoutMeta[] | null;
+  @Input() signoutMeta!: BoarderSignoutMeta | null;
+  selectedBoarder!: BoarderSignoutMeta | null;
+  signoutFields!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signoutFields = this.fb.group({
+      destination: ['', Validators.required],
+      transport: ['', Validators.required],
+      carDetail: ['', Validators.required],
+    });
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.signoutMeta && this.signoutMeta && this.signout) {
+      this.fillFields(this.signout);
+    }
+  }
+
+  selectBoarder(boarderMeta: NamedItem){
+    this.selectedBoarder = boarderMeta as BoarderSignoutMeta;
+  }
+
+  fillFields(signout: StudentSignout): void {
+    this.selectedBoarder = this.signoutMeta;
+    this.signoutFields.controls.destination.setValue(signout.destination);
+    this.signoutFields.controls.transport.setValue(signout.transport);
+    if (signout.transport === 'Car') {
+      this.signoutFields.controls.carDetail.setValue(signout.transportNote);
+    }
+  }
+
+  get transportOptions(): string[]{
+    const options: TransportMethod[] = [];
+    if(this.selectedBoarder){
+      if(this.selectedBoarder.permissions.canWalk){
+        options.push('Walk');
+      }
+      if(this.selectedBoarder.permissions.canBike){
+        options.push('Bike');
+      }
+      if(this.selectedBoarder.permissions.canCar){
+        options.push('Car');
+      }
+    }
+    return options;
+  }
+
+  get editedSignout(): StudentSignout {
+    let signout: StudentSignout;
+    if (this.signout) {
+      signout = {
+        ...this.signout,
+        destination: this.signoutFields.controls.destination.value,
+        transport: this.signoutFields.controls.transport.value,
+      };
+    } else {
+      let student;
+      if (this.selectedBoarder) {
+        student = {
+          name: this.selectedBoarder.name,
+          uid: this.selectedBoarder.uid,
+        };
+      } else {
+        student = {
+          name: '',
+          uid: '',
+        };
+      }
+      signout = {
+        student,
+        destination: this.signoutFields.controls.destination.value,
+        transport: this.signoutFields.controls.transport.value,
+        timeOut: new Date().toISOString(),
+        isCurrentlyOut: true,
+        uid: '',
+      };
+    }
+    if (signout.transport === 'Car') {
+      signout.transportNote = this.signoutFields.controls.carDetail.value;
+    }
+    return signout;
+  }
 }
