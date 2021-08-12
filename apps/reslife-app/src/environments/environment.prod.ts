bddba4b5b830e4cff9f73814650f887f97f7ab9a
atinybeardedman <sedickinson9123@gmail.com>
2021-08-12 09:57:13 -0400

    fix: add firebase to production env

--- apps/reslife-app/src/environments/environment.prod.ts
+++ apps/reslife-app/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: "AIzaSyBDnop9prdY-550K5vaBCQ5IqnMOS8quUQ",
+    authDomain: "reslife-staging.firebaseapp.com",
+    projectId: "reslife-staging",
+    storageBucket: "reslife-staging.appspot.com",
+    messagingSenderId: "600090189906",
+    appId: "1:600090189906:web:0beb87dd4bc6faf714bf8b"
+  }
 };
