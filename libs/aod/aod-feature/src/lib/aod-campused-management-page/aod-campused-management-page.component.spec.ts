6303cf0aa50f87423d0e2863cedaa7b9f672c1f6
atinybeardedman <sedickinson9123@gmail.com>
2021-08-19 11:31:22 -0400

    tests: add passing test to campused page

--- libs/aod/aod-feature/src/lib/aod-campused-management-page/aod-campused-management-page.component.spec.ts
+++ libs/aod/aod-feature/src/lib/aod-campused-management-page/aod-campused-management-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AodSignoutDataService, SharedAodDataService } from '@reslife/aod-data-access';
+import { MockModule, MockProvider } from 'ng-mocks';
 
 import { AodCampusedManagementPageComponent } from './aod-campused-management-page.component';
+import { AodCampusedManagementPageModule } from './aod-campused-management-page.module';
 
 describe('AodCampusedManagementPageComponent', () => {
   let component: AodCampusedManagementPageComponent;
@@ -8,7 +11,14 @@ describe('AodCampusedManagementPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AodCampusedManagementPageComponent ]
+      declarations: [ AodCampusedManagementPageComponent ],
+      imports: [
+        MockModule(AodCampusedManagementPageModule)
+      ],
+      providers: [
+        MockProvider(SharedAodDataService),
+        MockProvider(AodSignoutDataService),
+      ]
     })
     .compileComponents();
   });
