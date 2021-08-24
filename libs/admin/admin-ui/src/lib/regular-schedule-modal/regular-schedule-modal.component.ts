c7a1919221b18779a90a8bd66284aebb0f715de6
atinybeardedman <sedickinson9123@gmail.com>
2021-08-24 10:40:01 -0400

    feat: regular schedule modal component

--- libs/admin/admin-ui/src/lib/regular-schedule-modal/regular-schedule-modal.component.ts
+++ libs/admin/admin-ui/src/lib/regular-schedule-modal/regular-schedule-modal.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, EventEmitter, Input, Output, SimpleChanges, OnChanges } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { DAYNAMES, ScheduleItem } from '@reslife/admin-model';
+import { getAcademicYear } from '@reslife/utils';
 
 @Component({
   selector: 'reslife-regular-schedule-modal',
@@ -6,11 +9,70 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['./regular-schedule-modal.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegularScheduleModalComponent implements OnInit {
+export class RegularScheduleModalComponent implements OnChanges{
+  @Input() title!: string;
+  @Input() scheduleItem!: ScheduleItem | null;
+  @Input() academicYear = getAcademicYear();
 
-  constructor() { }
+  @Output() save = new EventEmitter<ScheduleItem>();
+  scheduleForm: FormGroup;
+  daysArray: FormArray;
+  dayNames = DAYNAMES;
+  constructor(fb: FormBuilder) {
+    this.daysArray = fb.array([]);
+    for(const _day of DAYNAMES){
+      this.daysArray.push(fb.control(false));
+    }
+    this.scheduleForm = fb.group({
+      name: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      days: this.daysArray
+    })
+   }
 
-  ngOnInit(): void {
-  }
+   ngOnChanges(changes: SimpleChanges): void {
+    if(changes.scheduleItem && this.scheduleItem){
+      const nameCtrl = this.scheduleForm.controls.name;
+      nameCtrl.setValue(this.scheduleItem.name);
+      nameCtrl.disable();
+
+      const {startTime, endTime} = this.scheduleForm.controls;
+      startTime.setValue(this.scheduleItem.startTime);
+      endTime.setValue(this.scheduleItem.endTime);
+
+      for(const dayNum of this.scheduleItem.days){
+        this.daysArray.at(dayNum).setValue(true);
+      }
+    }
+   }
+
+   get isAnyDaySelected(): boolean {
+     return this.daysArray.controls.some(c => c.value === true);
+   }
+
+
+   get editedScheduleItem(): ScheduleItem {
+     const {name, startTime, endTime} = this.scheduleForm.controls;
+     const days = [];
+     for(const [index, dayCtrl] of this.daysArray.controls.entries()){
+      if(dayCtrl.value === true){
+        days.push(index);
+      }
+     }
+     const doc: ScheduleItem = {
+       name: name.value,
+       startTime: startTime.value,
+       endTime: endTime.value,
+       days,
+       academicYear: this.academicYear,
+       uid: ''
+     };
+     if(this.scheduleItem){
+       doc.uid = this.scheduleItem.uid;
+     }
+     return doc;
+
+   }
 
 }
--- libs/admin/admin-ui/src/lib/regular-schedule-modal/regular-schedule-modal.module.ts
+++ libs/admin/admin-ui/src/lib/regular-schedule-modal/regular-schedule-modal.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegularScheduleModalComponent } from './regular-schedule-modal.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 
@@ -9,7 +16,15 @@ import { RegularScheduleModalComponent } from './regular-schedule-modal.componen
     RegularScheduleModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
   exports: [
     RegularScheduleModalComponent
