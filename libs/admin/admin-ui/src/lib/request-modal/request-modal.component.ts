6a58f3c3390a2de54ea8bde4105ace330b1a3dc2
atinybeardedman <sedickinson9123@gmail.com>
2021-08-27 11:33:41 -0400

    feat: add request modal

--- libs/admin/admin-ui/src/lib/request-modal/request-modal.component.ts
+++ libs/admin/admin-ui/src/lib/request-modal/request-modal.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { LeaveStayRequest } from '@reslife/admin-model';
 
 @Component({
   selector: 'reslife-request-modal',
   templateUrl: './request-modal.component.html',
   styleUrls: ['./request-modal.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RequestModalComponent implements OnInit {
+export class RequestModalComponent {
+  @Input() request!: LeaveStayRequest | null;
+  @Input() readOnly = false;
+  @Output() save = new EventEmitter<LeaveStayRequest>();
 
-  constructor() { }
 
-  ngOnInit(): void {
+  get isValid(): boolean {
+    if (this.request?.status === 'Rejected') {
+      return typeof this.request.rejectionReason !== 'undefined' && this.request.rejectionReason !== '';
+    }
+    return this.request?.status === 'Approved';
   }
 
+  emitRequest(): void {
+    if (this.request) {
+      this.save.emit(this.request);
+    }
+  }
 }
--- libs/admin/admin-ui/src/lib/request-modal/request-modal.module.ts
+++ libs/admin/admin-ui/src/lib/request-modal/request-modal.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestModalComponent } from './request-modal.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
 
 
 
@@ -9,7 +17,16 @@ import { RequestModalComponent } from './request-modal.component';
     RequestModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     RequestModalComponent
