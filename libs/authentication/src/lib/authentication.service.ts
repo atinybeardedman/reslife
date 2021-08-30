7f64b1a0785885ce9bfc359b9658317976e16983
atinybeardedman <sedickinson9123@gmail.com>
2021-08-30 15:21:04 -0400

    feat: add error handling

--- libs/authentication/src/lib/authentication.service.ts
+++ libs/authentication/src/lib/authentication.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import {  Observable, of } from 'rxjs';
 import { StaffMember } from '@reslife/admin-model';
 import firebase from 'firebase/app';
-import { map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,9 +12,11 @@ export class AuthenticationService {
   constructor(private auth: AngularFireAuth, private af: AngularFirestore) {}
 
   async login(): Promise<void> {
-    await this.auth.signInWithPopup(
-      new firebase.auth.GoogleAuthProvider()
-    );
+ 
+      await this.auth.signInWithPopup(
+        new firebase.auth.GoogleAuthProvider()
+      );
+    
   }
 
   async logout() {
@@ -25,7 +27,12 @@ export class AuthenticationService {
     return this.auth.user.pipe(
       switchMap((user) => {
         if (user) {
-          return this.af.doc<StaffMember>(`staff/${user.uid}`).valueChanges();
+          return this.af.doc<StaffMember>(`staff/${user.uid}`).valueChanges().pipe(
+            catchError(err => {
+              console.log(err);
+              return of(null)})
+          );
+
         } else {
           return of(null);
         }
