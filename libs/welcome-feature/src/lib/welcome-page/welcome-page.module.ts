f047d81d07b2c2f44651ef5b8784f0871ab14bff
atinybeardedman <sedickinson9123@gmail.com>
2021-08-30 09:46:56 -0400

    fix: improve styling of welcome

--- libs/welcome-feature/src/lib/welcome-page/welcome-page.module.ts
+++ libs/welcome-feature/src/lib/welcome-page/welcome-page.module.ts
@@ -2,10 +2,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomePageComponent } from './welcome-page.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout'
 @NgModule({
   declarations: [WelcomePageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, FlexLayoutModule],
   exports: [WelcomePageComponent],
 })
 export class WelcomePageModule {}
