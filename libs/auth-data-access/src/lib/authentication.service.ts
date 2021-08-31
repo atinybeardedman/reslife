092acc9798ed9e7ebe98b92f3b022130be56f714
atinybeardedman <sedickinson9123@gmail.com>
2021-08-31 16:03:36 -0400

    feat: add domain to sign-in

--- libs/auth-data-access/src/lib/authentication.service.ts
+++ libs/auth-data-access/src/lib/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
-import {  Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { StaffMember } from '@reslife/admin-model';
 import firebase from 'firebase/app';
 import { catchError, map, switchMap } from 'rxjs/operators';
@@ -12,11 +12,11 @@ export class AuthenticationService {
   constructor(private auth: AngularFireAuth, private af: AngularFirestore) {}
 
   async login(): Promise<void> {
- 
-      await this.auth.signInWithPopup(
-        new firebase.auth.GoogleAuthProvider()
-      );
-    
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.setCustomParameters({
+      hd: 'oakwoodfriends.org',
+    });
+    await this.auth.signInWithPopup(provider);
   }
 
   async logout() {
@@ -27,21 +27,24 @@ export class AuthenticationService {
     return this.auth.user.pipe(
       switchMap((user) => {
         if (user) {
-          return this.af.doc<StaffMember>(`staff/${user.uid}`).valueChanges().pipe(
-            catchError(err => {
-              console.log(err);
-              return of(null)})
-          );
-
+          return this.af
+            .doc<StaffMember>(`staff/${user.uid}`)
+            .valueChanges()
+            .pipe(
+              catchError((err) => {
+                console.log(err);
+                return of(null);
+              })
+            );
         } else {
           return of(null);
         }
       }),
-      map(user => {
-        if(typeof user === 'undefined'){
-          return null
+      map((user) => {
+        if (typeof user === 'undefined') {
+          return null;
         }
-        return user
+        return user;
       })
     );
   }
