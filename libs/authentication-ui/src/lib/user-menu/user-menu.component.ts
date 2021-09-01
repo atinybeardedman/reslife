0420c01c7fbe9bc213f08aa8a54502b37b114f0d
atinybeardedman <sedickinson9123@gmail.com>
2021-09-01 08:49:26 -0400

    feat: add logout to request

--- libs/authentication-ui/src/lib/user-menu/user-menu.component.ts
+++ libs/authentication-ui/src/lib/user-menu/user-menu.component.ts
@@ -1,13 +1,32 @@
-import { Component, ChangeDetectionStrategy, Input, EventEmitter, Output } from '@angular/core';
-import { StaffMember } from '@reslife/admin-model'
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { StaffMember } from '@reslife/admin-model';
+import firebase from 'firebase';
 @Component({
   selector: 'reslife-user-menu',
   templateUrl: './user-menu.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserMenuComponent  {
+export class UserMenuComponent {
+  @Input() user!: StaffMember | firebase.User | null;
+  @Output() logout = new EventEmitter<void>();
 
- @Input() user!: StaffMember;
- @Output() logout = new EventEmitter<void>();
-
+  private isStaff(user: StaffMember | firebase.User): user is StaffMember {
+    return typeof (<StaffMember>user).roles !== 'undefined';
+  }
+  get name(): string {
+    if (this.user) {
+      if (this.isStaff(this.user)) {
+        return this.user.name;
+      } else {
+        return (<firebase.User>this.user).displayName as string;
+      }
+    }
+    return '';
+  }
 }
