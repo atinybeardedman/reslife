85876d1bddb038a37190739dc440ab9c63ee9392
atinybeardedman <sedickinson9123@gmail.com>
2021-09-05 16:25:56 -0400

    feat: add boarder update logic

--- functions/src/boarders.ts
+++ functions/src/boarders.ts
@@ -1,7 +1,17 @@
 import * as fbadmin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import { Boarder } from './types';
-import { getDateFromDateString, getDateString } from './utils/date';
+
+import {
+  Boarder,
+  CheckInItem,
+  CheckInRecord,
+  ExcusedRecord,
+} from './types';
+import {
+  addToDatestring,
+  getDateFromDateString,
+  getDateString,
+} from './utils/date';
 import { getCheckinsByDateRange } from './utils/excusals';
 
 const toggleBoarderStatus = async () => {
@@ -12,90 +22,292 @@ const toggleBoarderStatus = async () => {
     .where('startDate', '<=', today)
     .where('isActive', '==', false)
     .get()
-    .then(snap => snap.docs);
+    .then((snap) => snap.docs);
 
-    const activeBoarders = await fbadmin
+  const activeBoarders = await fbadmin
     .firestore()
     .collection('boarders')
     .where('endDate', '<=', today)
     .where('isActive', '==', true)
     .get()
-    .then(snap => snap.docs);
+    .then((snap) => snap.docs);
 
-    if(inActiveBoarders.length > 0 || activeBoarders.length > 0){
-        const batch = fbadmin.firestore().batch();
-    
-        for(const b of inActiveBoarders){
-            batch.update(b.ref, {isActive: true});
-        }
-    
-        for(const b of activeBoarders){
-            batch.update(b.ref, {isActive: false});
-        }
-        try {
-            await batch.commit();
+  if (inActiveBoarders.length > 0 || activeBoarders.length > 0) {
+    const batch = fbadmin.firestore().batch();
 
-        } catch (err){
-            console.log(err);
-        }
+    for (const b of inActiveBoarders) {
+      batch.update(b.ref, { isActive: true });
     }
 
+    for (const b of activeBoarders) {
+      batch.update(b.ref, { isActive: false });
+    }
+    try {
+      await batch.commit();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 };
 
-const onBoarderCreate = functions.firestore.document('boarders/{boarderID}').onCreate(async (snap) => {
+const is7DayOnly = (
+  checkIn: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>
+): boolean => {
+  const day = getDateFromDateString(checkIn.get('date')).getDay();
+  return day >= 5 || (day === 0 && checkIn.get('end') <= '19:00');
+};
+
+const getCheckInRefWithItem = (
+  checkIn: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>,
+  boarder: Boarder
+): {
+  ref: FirebaseFirestore.DocumentReference;
+  item: CheckInRecord | ExcusedRecord;
+} => {
+  let item: CheckInItem | ExcusedRecord;
+  let ref: FirebaseFirestore.DocumentReference;
+  const shouldExcuse5Day = !is7DayOnly(checkIn);
+  if (boarder.type === '5 Day' && shouldExcuse5Day) {
+    ref = checkIn.ref.collection('excused').doc(boarder.uid);
+    item = {
+      name: boarder.name,
+      uid: boarder.uid,
+      note: '5 day',
+    };
+  } else {
+    ref = checkIn.ref.collection('expected').doc(boarder.uid);
+    item = {
+      name: boarder.name,
+      uid: boarder.uid,
+    };
+  }
+  return {
+    item,
+    ref,
+  };
+};
+
+const onBoarderCreate = functions.firestore
+  .document('boarders/{boarderID}')
+  .onCreate(async (snap) => {
     const boarder = snap.data() as Boarder;
-    const checkIns = await getCheckinsByDateRange(boarder.startDate, boarder.endDate);
+    const checkIns = await getCheckinsByDateRange(
+      boarder.startDate,
+      boarder.endDate
+    );
     const batch = fbadmin.firestore().batch();
-    for(const checkIn of checkIns.docs){
-        if(boarder.type === '5 Day'){
-            const day = getDateFromDateString(checkIn.get('date')).getDay();
-            if(day >= 5 || (day === 0 && checkIn.get('start') >= '19:00')){
-                batch.set(checkIn.ref.collection('excused').doc(boarder.uid), {
-                    uid: boarder.uid,
-                    name: boarder.name,
-                    note: '5 Day'
-                });
-                continue;
-            }
-        } 
-        batch.set(checkIn.ref.collection('expected').doc(boarder.uid), {
-            uid: boarder.uid,
-            name: boarder.name
-        })
+    for (const checkIn of checkIns.docs) {
+      const { ref, item } = getCheckInRefWithItem(checkIn, boarder);
+      batch.set(ref, item);
     }
     try {
-        await batch.commit();
-        await fbadmin.auth().createUser({
-            email: boarder.email,
-            displayName: boarder.name,
-            uid: boarder.uid
-        })
-    } catch(err){
-        console.log(err);
+      await batch.commit();
+      await fbadmin.auth().createUser({
+        email: boarder.email,
+        displayName: boarder.name,
+        uid: boarder.uid,
+      });
+    } catch (err) {
+      console.log(err);
     }
-});
+  });
 
-const onBoarderDelete = functions.firestore.document('boarders/{boarderID}').onDelete(async (snap) => {
+const onBoarderDelete = functions.firestore
+  .document('boarders/{boarderID}')
+  .onDelete(async (snap) => {
     const boarder = snap.data() as Boarder;
-    const checkIns = await getCheckinsByDateRange(boarder.startDate, boarder.endDate);
+    const checkIns = await getCheckinsByDateRange(
+      boarder.startDate,
+      boarder.endDate
+    );
     const batch = fbadmin.firestore().batch();
-    for(const checkIn of checkIns.docs){
-        batch.delete(checkIn.ref.collection('expected').doc(boarder.uid))
-        batch.delete(checkIn.ref.collection('excused').doc(boarder.uid))
+    for (const checkIn of checkIns.docs) {
+      batch.delete(checkIn.ref.collection('expected').doc(boarder.uid));
+      batch.delete(checkIn.ref.collection('excused').doc(boarder.uid));
     }
     try {
-        await batch.commit();
-    } catch(err){
-        console.log(err);
+      await batch.commit();
+    } catch (err) {
+      console.log(err);
     }
-});
+  });
 
+const onBoarderUpdate = functions.firestore
+  .document('boarders/{boarderId}')
+  .onUpdate(async (snap) => {
+    const before = snap.before.data() as Boarder;
+    const after = snap.after.data() as Boarder;
+    const datesChanged =
+      before.startDate !== after.startDate || before.endDate !== after.endDate;
+    if (datesChanged) {
+      if (before.startDate !== after.startDate) {
+        if (before.startDate < after.startDate) {
+          // starting earlier than before, add to check-ins (inclusive)
+          const checkIns = await getCheckinsByDateRange(
+            before.startDate,
+            after.startDate
+          );
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs) {
+            const { ref, item } = getCheckInRefWithItem(checkIn, after);
+            batch.set(ref, item);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          // starting later than expected, remove check-ins (exclusive ending)
+          const end = addToDatestring(after.startDate, -1);
+          const checkIns = await getCheckinsByDateRange(before.startDate, end);
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs) {
+            const { ref, item } = getCheckInRefWithItem(checkIn, after);
+            batch.set(ref, item);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+
+      if (before.endDate !== after.endDate) {
+        if (before.endDate < after.endDate) {
+          // ending later than before, add more check-ins (inclusive)
+          const checkIns = await getCheckinsByDateRange(
+            before.endDate,
+            after.endDate
+          );
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs) {
+            const { ref, item } = getCheckInRefWithItem(checkIn, after);
+            batch.set(ref, item);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          // ending earlier than before, remove check-ins (exclusive starting)
+          const start = addToDatestring(after.endDate, 1);
+          const checkIns = await getCheckinsByDateRange(start, after.endDate);
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs) {
+            const { ref, item } = getCheckInRefWithItem(checkIn, after);
+            batch.set(ref, item);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+    } else {
+      // start and end dates haven't changed so need to affect all relevant check-ins
+      if (before.type !== after.type) {
+        // should adjust check-ins. But only from current date forward
+        const today = getDateString();
+        if (before.type === '5 Day') {
+          // now 7 day, needs to shift from excused to expected for all weekends
+          const checkIns = await getCheckinsByDateRange(today, after.endDate);
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs.filter((d) => is7DayOnly(d))) {
+            const expectedRef = checkIn.ref
+              .collection('expected')
+              .doc(after.uid);
+            const expectedDoc: CheckInItem = {
+              uid: after.uid,
+              name: after.name,
+            };
+            batch.set(expectedRef, expectedDoc);
+            const excusedRef = checkIn.ref.collection('excused').doc(after.uid);
+            batch.delete(excusedRef);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        } else {
+          // now 5 day, needs to shifdt from expected to excused for all weekends
+          const checkIns = await getCheckinsByDateRange(today, after.endDate);
+          const batch = fbadmin.firestore().batch();
+          for (const checkIn of checkIns.docs.filter((d) => is7DayOnly(d))) {
+            const expectedRef = checkIn.ref
+              .collection('expected')
+              .doc(after.uid);
+
+            batch.delete(expectedRef);
+
+            const excusedRef = checkIn.ref.collection('excused').doc(after.uid);
+            const excusalDoc: ExcusedRecord = {
+              uid: after.uid,
+              name: after.name,
+              note: '5 day',
+            };
+            batch.set(excusedRef, excusalDoc);
+          }
+          try {
+            await batch.commit();
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      }
+
+      if (before.name !== after.name) {
+        const expectedSnap = await fbadmin
+          .firestore()
+          .collectionGroup('expected')
+          .where('uid', '==', after.uid)
+          .get();
+        const checkedSnap = await fbadmin
+          .firestore()
+          .collectionGroup('checked')
+          .where('uid', '==', after.uid)
+          .get();
+        const excusedSnap = await fbadmin
+          .firestore()
+          .collectionGroup('excused')
+          .where('uid', '==', after.uid)
+          .get();
+        try {
+          await updateAllDocs(expectedSnap, { name: after.name });
+          await updateAllDocs(checkedSnap, { name: after.name });
+          await updateAllDocs(excusedSnap, { name: after.name });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  });
+
+const updateAllDocs = async (
+  snap: FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>,
+  update: Partial<Boarder>
+): Promise<void> => {
+  const batch = fbadmin.firestore().batch();
+  for (const expectedDoc of snap.docs) {
+    batch.update(expectedDoc.ref, update);
+  }
+  try {
+    await batch.commit();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const triggerableFns = {
-    toggleBoarderStatus
-}
+  toggleBoarderStatus,
+};
 
 export const backgroundFns = {
-    onBoarderCreate,
-    onBoarderDelete
+  onBoarderCreate,
+  onBoarderDelete,
+  onBoarderUpdate,
 };
